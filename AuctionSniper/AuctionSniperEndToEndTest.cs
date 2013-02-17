a72a6b43cd25b950333ab330c714c15cebb398dc
Joe Leo <joseph.leo3@gmail.com>
2013-02-17 16:34:53 -0500

    Initial commit of AuctionSniper

---
+++ AuctionSniper/AuctionSniperEndToEndTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionSniper
+{
+    class AuctionSniperEndToEndTest
+    {
+        static FakeAuctionServer Auction = new FakeAuctionServer("item-54321");
+        static ApplicationRunner Application = new ApplicationRunner();
+
+    }
+    
+    class SniperJoinsAuctionUntilAuctionCloses()
+    {
+        Auction.StartSellingItem();
+        Application.StartBiddingIn(Auction);
+        Auction.HasReceivedJoinRequestFromSniper();
+        Auction.AnnounceClosed();
+        Application.ShowsSniperHasLostAuction();
+    }
+}
+
+/* TODO
+-- mspec
+-- resharper
+-- vsvim
+*/
