db3c7206649d79933245f0b795a3818d719eea09
Joe Leo <joseph.leo3@gmail.com>
2013-02-18 15:49:04 -0500

    Removing the FakeAuctionServer as a separate class; we don't need it yet     End-to-end test built - empty classes created in order to keep the project compiling

--- AuctionSniper/AuctionSniperEndToEndTest.cs
+++ AuctionSniper/AuctionSniperEndToEndTest.cs
@@ -3,28 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Machine.Specifications;
 
 namespace AuctionSniper
 {
-    class AuctionSniperEndToEndTest
+    public class AuctionSniperEndToEndTest
     {
         static FakeAuctionServer Auction = new FakeAuctionServer("item-54321");
         static ApplicationRunner Application = new ApplicationRunner();
 
+        private It will_execute_each_step = () =>
+            {
+                Auction.StartSellingItem();
+                Application.StartBiddingIn(Auction);
+                Auction.HasReceivedJoinRequestFromSniper();
+                Auction.AnnounceClosed();
+                Application.ShowsSniperHasLostAuction();
+            };
     }
-    
-    class SniperJoinsAuctionUntilAuctionCloses()
+
+    internal class ApplicationRunner
     {
-        Auction.StartSellingItem();
-        Application.StartBiddingIn(Auction);
-        Auction.HasReceivedJoinRequestFromSniper();
-        Auction.AnnounceClosed();
-        Application.ShowsSniperHasLostAuction();
+        public void StartBiddingIn(FakeAuctionServer auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowsSniperHasLostAuction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class FakeAuctionServer
+    {
+        public FakeAuctionServer(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSellingItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HasReceivedJoinRequestFromSniper()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AnnounceClosed()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
-
-/* TODO
--- mspec
--- resharper
--- vsvim
-*/
